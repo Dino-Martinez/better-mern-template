70e8fd53262f66415281d322d0c18334b6388b06
Dino-Martinez <dino.martinez.514@gmail.com>
2022-05-12 15:03:40 -0400

    [Edit] api/app.js
    Routed all non-api routes to a catch-all for future front-end display. Implemented authentication middleware and database setup

--- api/app.js
+++ api/app.js
@@ -3,7 +3,11 @@ const cors = require('cors')
 const helmet = require('helmet')
 const morgan = require('morgan')
 const compression = require('compression')
+const cookies = require('cookie-parser')
+const jwt = require('jsonwebtoken')
 require('dotenv').config()
+require('./utils/dbConnection').config()
+
 const app = express()
 
 app.use(morgan('common'))
@@ -11,12 +15,31 @@ app.use(helmet())
 app.use(cors())
 app.use(express.urlencoded({ extended: true })) // Handling form data
 app.use(express.json()) // Handling JSON data
+app.use(cookies())
 app.use(compression())
+
+// Middleware for checking user authentication
+const checkAuth = (req, res, next) => {
+  if (
+    typeof req.cookies.authToken === 'undefined' ||
+    req.cookies.authToken === null
+  ) {
+    req.user = null
+  } else {
+    const token = req.cookies.authToken
+    const decodedToken = jwt.decode(token, { complete: true }) || {}
+    req.user = decodedToken.payload
+  }
+
+  next()
+}
+
+app.use(checkAuth)
 
 const routes = require('./routes')
 app.use('/api', routes)
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   res.send('hello')
 })
 
