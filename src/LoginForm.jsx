198f5d7a308e3d799e5c35f6a0a5158f49ba4273
Dino-Martinez <dino.martinez.514@gmail.com>
2022-05-12 17:33:27 -0400

    [Edit] App.jsx : [Add] AuthProvider.jsx LoginForm.jsx
    Implemented a React Context for global auth state so all components have the option to subscribe to user authentication status.

---
+++ src/LoginForm.jsx
@@ -0,0 +1,19 @@
+import React, { useContext, useState } from 'react'
+import { AuthContext } from './AuthProvider'
+function LoginForm () {
+  const { user, signup, login, logout } = useContext(AuthContext)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  return (
+    <div className='App'>
+      <input type='text' value={username} onChange={e => setUsername(e.target.value)} placeholder='Username' />
+      <input type='password' value={password} onChange={e => setPassword(e.target.value)} placeholder='Password' />
+      <button onClick={e => signup(username, password)}>Sign up</button>
+      <button onClick={e => login(username, password)}>Log in</button>
+      <button onClick={logout}>Log Out</button>
+      {user && <p>Logged in</p>}
+    </div>
+  )
+}
+
+export default LoginForm
