9fdb161f29a15ac77b86c69489b608f46970e68b
Dino-Martinez <dino.martinez.514@gmail.com>
2022-05-14 17:47:36 -0400

    [Edit] Nav.jsx LoginForm.jsx

--- src/LoginForm.jsx
+++ src/LoginForm.jsx
@@ -1,16 +1,25 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
 import { AuthContext } from './AuthProvider'
 function LoginForm () {
-  const { signup, login, logout } = useContext(AuthContext)
+  const { user, loading, signup, login } = useContext(AuthContext)
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const navigate = useNavigate()
+  const { state } = useLocation()
+  useEffect(() => {
+    if (user && !loading) { navigate(state.redirectUrl) }
+  }, [user, loading, navigate, state])
   return (
-    <div className='App'>
-      <input type='text' value={username} onChange={e => setUsername(e.target.value)} placeholder='Username' />
-      <input type='password' value={password} onChange={e => setPassword(e.target.value)} placeholder='Password' />
-      <button onClick={e => signup(username, password)}>Sign up</button>
-      <button onClick={e => login(username, password)}>Log in</button>
-      <button onClick={logout}>Log Out</button>
+    <div>
+      {!user && (
+        <>
+          <input type='text' value={username} onChange={e => setUsername(e.target.value)} placeholder='Username' />
+          <input type='password' value={password} onChange={e => setPassword(e.target.value)} placeholder='Password' />
+          <button onClick={e => signup(username, password)}>Sign up</button>
+          <button onClick={e => login(username, password)}>Log in</button>
+        </>
+      )}
     </div>
   )
 }
